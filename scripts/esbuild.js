4f5236f61c86837a023b24df706bfb98625234dc
stevenlandis <stevenlandis@comcast.net>
2022-09-07 19:18:45 -0700

    add command for building with typescript

--- scripts/esbuild.js
+++
@@ -1,9 +0,0 @@
-require("esbuild").build({
-  entryPoints: ["src/index.ts"],
-  bundle: true,
-  minify: true,
-  // format: "esm",
-  // sourcemap: true,
-  outfile: "build/out.js",
-  // external: ['react', 'react-dom'],
-});
