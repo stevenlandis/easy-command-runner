bcdc16f406277cb3345e5df18ec03a4cd0ca4be7
stevenlandis <stevenlandis@comcast.net>
2022-09-18 11:29:37 -0700

    add prompt for changelog.md to release script

--- scripts/release-new-version.js
+++ scripts/release-new-version.js
@@ -8,26 +8,33 @@ async function main() {
   );
   let currentVersion = packageJsonObj.version;
 
-  let nextVersion = await new Promise((resolve) => {
-    let interface = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
-    interface.question(
-      `Current version: ${currentVersion}.\nNew version: `,
-      (answer) => {
-        resolve(answer);
-        interface.close();
-      }
-    );
+  let lineReader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
   });
+  async function input(prompt) {
+    return new Promise((resolve) => {
+      lineReader.question(prompt, (answer) => resolve(answer));
+    });
+  }
+
+  let updatedChangelog = await input("Have you updated CHANGELOG.md? [y/n] ");
+  if (updatedChangelog.trim().toLowerCase() !== "y") {
+    process.exit(1);
+  }
+
+  let nextVersion = await input(
+    `Current version: ${currentVersion}.\nNew version: `
+  );
+  lineReader.close();
 
   await fs.promises.writeFile(
     "package.json",
     JSON.stringify({ ...packageJsonObj, version: nextVersion }, null, 2) + "\n"
   );
 
-  await cmd("yarn", "build-release").runDebug();
+  await cmd("yarn", "test").run();
+  await cmd("yarn", "build-release").run();
   await cmd.stdin().pipe("npm", "publish").run();
 
   await cmd(
